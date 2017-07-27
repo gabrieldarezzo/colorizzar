75bc2b1c9e852b5cb321936cb769d1f4702e51c2
preetender <cesinhagutierres@gmail.com>
2017-07-26 21:15:47 -0400

    - Classe formatada utilizando  padrão SRP - Melhorias na legibilidade - Metodo com o padrão Single Responsability

--- src/Colorizzar/UploadFile.php
+++ src/Colorizzar/UploadFile.php
@@ -65,33 +65,57 @@ class UploadFile
     */
     public function upload(array $file, $customNameFile = '')
     {
-        if (count($file) == 0) {
-            return false;
-        }
-            
-        if (!file_exists($this->UploadPath)) {
-            mkdir($this->UploadPath);
-        }
+        static::handle();
 
         $name = (($customNameFile == '') ? $file['arquivo']['name'] : $customNameFile);
         $tmp_name = $file['arquivo']['tmp_name'];
         $this->setCustomNameFile($name);
         
-        if ($file['arquivo']['error'] == UPLOAD_ERR_OK) {
-            $upload_dir = $this->UploadPath . DIRECTORY_SEPARATOR . $name;
+        $upload_dir = $this->UploadPath . DIRECTORY_SEPARATOR . $name;
 
-            $this->setFullPathFile($upload_dir);
-
+        $this->setFullPathFile($upload_dir);   
             
-            
-            if (!is_uploaded_file($tmp_name)) {
-                return copy($tmp_name, $upload_dir);
-            }
-            
-            return move_uploaded_file(
-                $tmp_name,
-                $upload_dir
-            );
+        if (!is_uploaded_file($tmp_name)) {
+            return static::copyImage($tmp_name, $upload_dir);
         }
+            
+        return static::moveImage($tmp_name, $upload_dir);
+    }
+
+	/**
+    * Responsible for conditional verification
+    * @return void
+    */
+    protected static function handle(array $file)
+    {
+        if( empty( $file ) ) {
+            return throw new \RuntimeException('File not defined');
+        }    
+        
+        if(! file_exists( $this->UploadPath) ) {
+            mkdir( $this->UploadPath );
+        }
+        
+        if ($file['arquivo']['error'] !== UPLOAD_ERR_OK) {
+            return throw new \Exception($file['arquivo']['error']); 
+        }
+    }
+
+    /**
+    * Copy image
+    * @return bool
+    */
+    protected static function copyImage($tmp_name, $dir)
+    {
+        return copy($tmp_name, $dir);
+    }
+    
+    /**
+    * Move image.
+    * @return bool
+    */
+    protected static function moveImage($tmp_name, $dir)
+    {
+        return move_uploaded_file($tmp_name, $dir);
     }
 }
